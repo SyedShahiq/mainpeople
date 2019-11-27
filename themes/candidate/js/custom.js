5f4a513b52a4280602717b4c09bd1feffca1a58f
Syed Shahiq <shahiqurrehman@gmail.com>
2019-11-27 21:34:07 +0500

    custom edit to main menu bar

--- themes/candidate/js/custom.js
+++ themes/candidate/js/custom.js
@@ -2,7 +2,8 @@
     "use strict";
 	jQuery(document).ready(function(){
         /*Review*/
-$('#header .container blockquote').text('Since 1982, a grassroots community action organization dedicated to social, racial and environmental justice');
+    $('#header .container blockquote').text('Since 1982, a grassroots community action organization dedicated to social, racial and environmental justice');
+    $('#block-system-main-menu .first a').addClass('icons icon-home active-trail');
 
         $('.c-review').click(function(){
             $('body,html').animate({scrollTop:900}, 800);
